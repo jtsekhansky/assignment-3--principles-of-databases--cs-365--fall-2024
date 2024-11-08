beb58c3a8c7b0665be65803e2223c36dac03bb2b
jtsekhansky <jtsekhansky@gmail.com>
2024-11-07 20:03:59 -0500

    more 2 space indents, capitalization

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -8,31 +8,31 @@ SET @key_str = UNHEX(SHA2('secret password secret password ', 256));
 SET @init_vector = X'C777333AAFABDFEA1988998985546789';
 
 CREATE TABLE websites (
-    website_name    VARCHAR(100)    ,
-    website_url     VARCHAR(128)    NOT NULL,
-    PRIMARY KEY (website_name)
+  website_name    VARCHAR(100)    ,
+  website_url     VARCHAR(128)    NOT NULL,
+  PRIMARY KEY (website_name)
 );
 
 CREATE TABLE users (
-    user_name        VARCHAR(45)    ,
-    first_name      VARCHAR(45)     NOT NULL,
-    last_name       VARCHAR(45)     NOT NULL,
-    email           VARCHAR(45)    NOT NULL,
-    PRIMARY KEY (user_name)
+  user_name        VARCHAR(45)    ,
+  first_name      VARCHAR(45)     NOT NULL,
+  last_name       VARCHAR(45)     NOT NULL,
+  email           VARCHAR(45)    NOT NULL,
+  PRIMARY KEY (user_name)
 );
 
 CREATE TABLE logins (
-    user_name        VARCHAR(45)    ,
-    website_name     VARCHAR(100)    NOT NULL,
-    password        VARBINARY(512)    NOT NULL,
-    comment         VARCHAR(2000)    NOT NULL,
-    update_time     DATETIME    NOT NULL,
-    PRIMARY KEY (user_name)
+  user_name        VARCHAR(45)    ,
+  website_name     VARCHAR(100)    NOT NULL,
+  password        VARBINARY(512)    NOT NULL,
+  comment         VARCHAR(2000)    NOT NULL,
+  update_time     DATETIME    NOT NULL,
+  PRIMARY KEY (user_name)
 );
 
-INSERT into websites
+INSERT INTO websites
   (website_name, website_url)
-values
+Values
   ("bing", "https://www.bing.com"),
   ("hartford", "https://www.hartford.edu"),
   ("cheshire high school", "https://www.cheshire.k12.ct.us"),
@@ -44,9 +44,9 @@ values
   ("Costco", "https://www.costco.com"),
   ("Amazon", "https://www.amazon.com");
 
-INSERT into users
+INSERT INTO users
   (user_name, first_name, last_name, email)
-values
+VALUES
   ("jtisawesome", "jacob", "tsekansky", "j@bing.com"),
   ("collegegood", "jacob", "tsekansky", "j@h0rtfurd.com"),
   ("schoolgreat", "david", "tsekansky", "d@cheshire.com"),
@@ -57,9 +57,9 @@ values
   ("saleshopstop", "brenna", "mullings", "mulls@stopshop.com"),
   ("savingbrands", "rahn", "kazowski", "kaz@brand.com"),
   ("warehouselifter", "lexi", "brimmings", "bestlift@atoz.com");
-INSERT into logins
+INSERT INTO logins
   (user_name, website_name, password, comment, update_time)
-values
+VALUES
   ("jtisawesome", "bing", aes_encrypt('dirtman', @key_str, @init_vector), "no comment", '2024-09-28 17:00:00'),
   ("collegegood", "hartford", aes_encrypt('roomdorm', @key_str, @init_vector), "nice school", '2024-09-28 17:00:00'),
   ("schoolgreat", "cheshire high school", aes_encrypt('moreclass', @key_str, @init_vector), "nice school", '2024-09-28 17:00:00'),
