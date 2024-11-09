7c26503a5d7f701d0baa334ff05a75ff3dbb6dc8
jtsekhansky <jtsekhansky@gmail.com>
2024-11-09 14:17:48 -0500

    style change: color of word

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 <?php
 require "includes/helpers.php";
 $connection_successful = checkConnection();
+$messagecolor = 'green';
+if(strpos( $connection_successful, 'fail' ) !== false){
+  $messagecolor = 'red';
+}
 ?>
 
 <!DOCTYPE HTML>
 <html lang = "en">
     <head>
-
+      <meta charset="utf-8">
+      <title>CS 365 - Assignment 3</title>
+      <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
         <header>
             <h1>Jacob Tsekhansky - Assignment 3</h1>
-            <p><?php echo $connection_successful ?></p>
+            <h2>Main Page</h2>
+            <p>
+                <font color=
+                "<?php echo $messagecolor ?>"
+                >
+            <?php echo $connection_successful ?></font>
+            </p>
         </header>
     </body>
 </html>
