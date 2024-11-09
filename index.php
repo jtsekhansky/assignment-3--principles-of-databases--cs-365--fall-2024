c059ff851034a343a202530c0fcec80612ed4239
jtsekhansky <jtsekhansky@gmail.com>
2024-11-08 21:20:04 -0500

    connection check verification (added html)

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 require "includes/helpers.php";
 $connection_successful = checkConnection();
+?>
+
+<!DOCTYPE HTML>
+<html lang = "en">
+    <head>
+
+    </head>
+    <body>
+        <header>
+            <h1>Jacob Tsekhansky - Assignment 3</h1>
+            <p><?php echo $connection_successful ?></p>
+        </header>
+    </body>
+</html>
